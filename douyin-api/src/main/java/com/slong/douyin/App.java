a8401358b99db012773f878dbcf6b1f2d460bb4b
my <y46605254@163.com>
2023-04-17 17:53:36 +0800

    初次上传

---
+++ douyin-api/src/main/java/com/slong/douyin/App.java
@@ -0,0 +1,13 @@
+package com.slong.douyin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
